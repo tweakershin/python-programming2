dc34f0ea7ead632605386723d510853b51130860
tweackers <tweatershin@gmail.com>
2019-05-30 18:15:07 +0900

    no message

---
+++ spyder_study/my_runfile.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 13:57:13 2019
+
+@author: student
+"""
+
+import mymodule
+import mypackages
+
+# total = mymodule.calc_sum(3, 3)
+
+
+
+
+
+if __name__ == '__main__':
+    total = mymodule.calc_sum(3, 3)
+    print(total)
+    
+    mypackages.module3.func1()
---
+++ spyder_study/mypackages/module3.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+def func1():
+    print("This is Func1")
+    
+    
